b6cf93e3b18c0e69ab33f42bebaf1f12571f044c
karlamedeiros04102006 <132077876+karlamedeiros04102006@users.noreply.github.com>
2024-04-26 08:57:44 -0300

    k

--- main.js
+++ main.js
@@ -20,9 +20,9 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-06-30T10:20:00");
-const tempoObjetivo2 = new Date("2024-06-13T14:30:00");
-const tempoObjetivo3 = new Date("2024-12-30T13:20:00");
-const tempoObjetivo4 = new Date("2025-02-01T18:00:00");
+const tempoObjetivo2 = new Date("2024-08-13T14:30:00");
+const tempoObjetivo3 = new Date("2024-11-04T13:20:00");
+const tempoObjetivo4 = new Date("2024-02-10T18:00:00");
 
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
